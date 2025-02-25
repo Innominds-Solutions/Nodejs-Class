b41238005c5b4c0c3eeed6efa3fa49dadc25695c
abijeetraut1 <abijeetraut2@gmial.com>
2025-02-25 22:07:49 +0545

    added listing based on the category

--- Controller/ViewController.js
+++ Controller/ViewController.js
@@ -28,11 +28,23 @@ exports.HomePage = CatchAsync(async (req, res) => {
                 ]
             } : {}
         },
+        {
+            $lookup: { // looks for that model which is linked to our category model ('from')
+                from: "categorymodels",
+                localField: "categoryId",
+                foreignField: "_id",
+                as: "category"
+            }
+        },
+        {
+            $unwind: { // flatten the document so that each element is accessible
+                path: "$category",
+                preserveNullAndEmptyArrays: true // if any of the category is not found the it gives null or empty constant
+            }
+        },
         { $skip: page ? Number((Number(page) - 1) * limit) : 0 },
         { $limit: limit },
     ]); // aggregate pipeline
-
-
 
 
     console.log(datas)
@@ -60,8 +72,21 @@ exports.AddBlogsPage = CatchAsync(async (req, res) => {
         isActive: true
     });
 
-
     res.render("Pages/Add_Blogs/Add_Blogs.ejs", {
         CategoryList
+    });
+})
+
+exports.GetOneBlogs = CatchAsync(async (req, res) => {
+    const slug = req.params.slug;
+
+    const blog = await blogsModel.findOne({
+        slug: slug,
+        isPublish: false
+    });
+
+    console.log(blog);
+    res.render("Pages/Blogs/Blogs.ejs", {
+        blog: blog
     });
 })
--- Router/ViewRouter.js
+++ Router/ViewRouter.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { HomePage, AboutPage, LoginPage, SignupPage, AddBlogsPage } = require("./../Controller/ViewController");
+const { HomePage, AboutPage, LoginPage, SignupPage, AddBlogsPage, GetOneBlogs } = require("./../Controller/ViewController");
 const { isAdminLoggedIn, isLoggedIn } = require("./../Controller/AuthController")
 
 router.get("/", HomePage);
 // router.get("/about", AboutPage);
+router.get("/:slug", GetOneBlogs);
 router.get("/login", LoginPage)
 router.delete("/logout", LoginPage)
 router.get("/signup", SignupPage)
